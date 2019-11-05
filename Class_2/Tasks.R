4560de61d36e1fb1c5f923a17d8d8a564c0e2ef2
sofigdl <garciasof@gmail.com>
2019-11-05 04:14:37 -0600

    add

--- Class_2/Tasks.R
+++ Class_2/Tasks.R
@@ -1,4 +1,8 @@
 #TASKS
+df_1<-data.frame(plot="location_name_1",measure1=runif(100)*1000, measure2=round(runif(100)*100),value=rnorm(100,2,1),ID=rep(LETTERS,100)[1:100])
+df_2<-data.frame(plot="location_name_2", measure1=runif(50)*100, measure2=round(runif(50)*10),value=rnorm(50), ID=rep(LETTERS,50)[1:50])
+df<-rbind(df_1,df_2) #combine two data frame row-wise, cbind for columns
+df
 
 df[,c('measure1','measure2')]
 df[66:70,c('value')]
