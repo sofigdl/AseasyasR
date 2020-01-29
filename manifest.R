21aa781245b17eda07daa6f2b43babde27518bf2
sofigdl <garciasof@gmail.com>
2020-01-29 23:07:02 +0100

    Update

---
+++ manifest.R
@@ -0,0 +1,10 @@
+
+library(rstudioapi)
+library(tidyverse) # ggplot2, dplyr, tidyr, readr, purrr, tibble
+library(magrittr) # pipes
+library(lintr) # code linting
+library(sf) # spatial data handling
+library(raster) # raster handling (needed for relief)
+library(viridis) # viridis color scale
+library(cowplot) # stack ggplots
+library(rmarkdown)
